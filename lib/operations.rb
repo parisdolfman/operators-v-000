dc6d305ec35cb1fd8a7c0bba7c690a59db98cc07
Paris Dolfman <parisdolfman@gmail.com>
2018-03-25 20:32:54 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,12 @@
 def unsafe?(speed)
-   if speed == < 40 || speed == < 60 
+   if speed == <40 || speed == <60
 		 true
    else false
- end 
+ end
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
